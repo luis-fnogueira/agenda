aff66044379a0abbe6e424788b044197e3a2f091
Luis Felipe Almeida Nogueira <luis.felipe32971@gmail.com>
2021-09-29 20:23:26 -0300

    Adicionado retorno de lista por JSON, retorno de Http404 e configurado timezone para pt-br.

--- core/views.py
+++ core/views.py
@@ -1,18 +1,25 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from core.models import Evento
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from datetime import datetime, timedelta
+from django.http.response import Http404, JsonResponse
+# Create your views here.
 
+# def index(request):
+#     return redirect('/agenda/')
+
+def handler404(request, exception):
+    return render(request, '404.html')
 
 def login_user(request):
     return render(request, 'login.html')
 
-
 def logout_user(request):
     logout(request)
     return redirect('/')
-
 
 def submit_login(request):
     if request.POST:
@@ -24,25 +31,36 @@ def submit_login(request):
             return redirect('/')
         else:
             messages.error(request, "Usuário ou senha inválido")
-        return redirect('/')
-
+    return redirect('/')
 
 @login_required(login_url='/login/')
 def lista_eventos(request):
     usuario = request.user
-    evento = Evento.objects.filter(usuario=usuario)
-    dados = {'eventos': evento}
+    data_atual = datetime.now() - timedelta(hours=1)
+    evento = Evento.objects.filter(usuario=usuario,
+                                   data_evento__gt=data_atual)
+    dados = {'eventos':evento}
     return render(request, 'agenda.html', dados)
 
+@login_required(login_url='/login/')
+def lista_eventos_historico(request):
+    usuario = request.user
+    data_atual = datetime.now()
+    evento = Evento.objects.filter(usuario=usuario,
+                                   data_evento__lt=data_atual)
+    dados = {'eventos':evento}
+    return render(request, 'historico.html', dados)
 
 @login_required(login_url='/login/')
 def evento(request):
     id_evento = request.GET.get('id')
     dados = {}
     if id_evento:
-        dados['Evento'] = Evento.objects.get(id=id_evento)
+        try:
+            dados['evento'] = Evento.objects.get(id=id_evento)
+        except Exception:
+            raise Http404()
     return render(request, 'evento.html', dados)
-
 
 @login_required(login_url='/login/')
 def submit_evento(request):
@@ -53,15 +71,15 @@ def submit_evento(request):
         usuario = request.user
         id_evento = request.POST.get('id_evento')
         if id_evento:
-            Evento.objects.filter(id=id_evento)
+            evento = Evento.objects.get(id=id_evento)
             if evento.usuario == usuario:
                 evento.titulo = titulo
                 evento.descricao = descricao
                 evento.data_evento = data_evento
                 evento.save()
             #Evento.objects.filter(id=id_evento).update(titulo=titulo,
-            #                                          data_evento=data_evento,
-            #                                         descricao=descricao)
+            #                                           data_evento=data_evento,
+            #                                           descricao=descricao)
         else:
             Evento.objects.create(titulo=titulo,
                                   data_evento=data_evento,
@@ -69,16 +87,21 @@ def submit_evento(request):
                                   usuario=usuario)
     return redirect('/')
 
-
 @login_required(login_url='/login/')
 def delete_evento(request, id_evento):
     usuario = request.user
-    evento = Evento.objects.get(id=id_evento)
+    try:
+        evento = Evento.objects.get(id=id_evento)
+    except Exception:
+        raise Http404()
     if usuario == evento.usuario:
         evento.delete()
+    else:
+        raise Http404()
     return redirect('/')
 
 
-'''def index(request):
-    return redirect('/agenda/')
-'''
+def json_lista_evento(request, id_usuario):
+    usuario = User.objects.get(id=id_usuario)
+    evento = Evento.objects.filter(usuario=usuario).values('id', 'titulo')
+    return JsonResponse(list(evento), safe=False)
